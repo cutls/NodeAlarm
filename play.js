c0801e8413cdbee9d550c1a4402e868eb57f8eb1
Cutls <web-pro@cutls.com>
2019-06-01 00:28:24 +0900

    fix

--- play.js
+++ play.js
@@ -2,7 +2,7 @@ console.log("child start");
 const child_process = require('child_process');
 
 process.on("message", function (msg) {
-    child_process.execSync('mpg321 '+msg)
+    child_process.execSync('mpg321 '+msg.message)
 });
 
 process.on("exit", function () {
